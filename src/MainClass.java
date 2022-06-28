71f429139e322b1e604b9bcc77a59d1e39d0eed8
wlsdn1778 <wlsdn1778@gmail.com>
2022-06-28 17:46:57 +0900

    6월 28일 화요일

---
+++ src/MainClass.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("헬로 월드");
+        // 클래스를 불러서 사용
+        NameSet ns = new NameSet();
+        ns.setName();  // 이름 설정 기능
+        // 아이템 설정
+        ns.setItem();
+//        System.out.println(ns.name + " 꺼져~");
+//        ns.name = "점심";
+//        System.out.println(ns.name + " 이리와~");
+        // 이름을 출력을 해보자
+        String name = ns.getName();    // 설정된 이름을 받을 수 있다
+        // 플레이 시작
+        PlayClass pc = new PlayClass();
+        int num = pc.play1(name);
+        num = pc.play2(num);
+
+        //아이템 리스트를 받아서 전달
+        ArrayList<ItemClass> list = ns.getItemList();
+        pc.play3(num, list);
+
+
+    }
+
+}
+
---
+++ src/PlayClass.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+
+public class PlayClass {
+    private Scanner sc;
+    // 생성자
+    public PlayClass(){
+        sc = new Scanner(System.in);
+    }
+//    public PlayClass(String name){}
+
+    // 메소드 만들기
+    public int play1(String name) {
+//        String name = "도라";
+        System.out.println(name + "이 설정 되었습니다.");
+        System.out.println(name + "이 여행을 떠난다");
+        // 가다가 퉁퉁이를 만났다
+        System.out.println(name + "이 길을 가다가 퉁퉁이를 만났다");
+        // 1. 싸운다 2. 도망간다
+        System.out.println("1. 싸운다 2. 도망간다");
+        System.out.print("입력: ");
+        return sc.nextInt();
+    }
+    public int play2(int num) {
+        int ret = 0;
+        // 만약에  num 1 이면 싸운다
+        if (num == 1) {
+            System.out.println("싸우자");
+            System.out.println("1. 공격 2. 아이템 사용 3. 방어");
+            System.out.print("입력: ");
+            ret = sc.nextInt();
+        } else if (num == 2) {
+            // 2면 도망간다
+            System.out.println();
+        } else {
+            // 잘못
+            System.out.println();
+        }
+        return ret;
+    }
+
+    public void play3(int num, ArrayList<ItemClass> list) {
+        if (num == 1) {
+            System.out.println("그냥 공격");
+        } else if (num == 2) {
+            System.out.println("아이템 사용");
+            //랜덤으로 아이템 사용하기(내가 설정한 아이템)
+            Random rnd = new Random();
+            int rnum = rnd.nextInt(list.size());//리스트 크기만큼에 대한 랜덤값 받기
+            System.out.println(list.get(rnum).getName()+" 아이템을 사용 했습니다.");
+            System.out.println("아이템 속성 : "+list.get(rnum).getAtt());
+        }
+    }
+}
